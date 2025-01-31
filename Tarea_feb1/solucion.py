31d5c03947d17519e22d61ea9b5f7921a5ad88b2
Andres Leon <aleonh@unal.edu.co>
2025-01-31 17:28:49 -0500

    Add punto 2

--- Tarea_feb1/solucion.py
+++ Tarea_feb1/solucion.py
@@ -5,16 +5,17 @@ import seaborn as sns
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.arima_process import ArmaProcess
+from statsmodels.tsa.stattools import adfuller
 
 # %%##########
 ## PUNTO 1
 ##############
 # Series de Inflación de alimentos y precipitación en el aeropuerto ElDorado
-inflacion = pd.read_excel("Tarea_feb1/data/inflacion_alimentos_mensual.xlsx", index_col=0,
+inflacion = pd.read_excel("./data/inflacion_alimentos_mensual.xlsx", index_col=0,
                           names=["Fecha", "Valor"],
                           parse_dates=["Fecha"], date_format="%d/%m/%Y", decimal=",")
 # noinspection PyTypeChecker
-precipitacion = pd.read_excel("Tarea_feb1/data/total_precipitacion.xlsx", sheet_name="Bogotá",
+precipitacion = pd.read_excel("./data/total_precipitacion.xlsx", sheet_name="Bogotá",
                               index_col=0, names=["Fecha", "Valor"], usecols="B:C", skiprows=5,
                               nrows=46, parse_dates=["Fecha"], date_format="%Y")
 
@@ -33,6 +34,28 @@ plt.show()
 # %%##########
 ## PUNTO 2
 ##############
+
+# MA de orden 5 y 10 para la serie de Inflación de Alimentos
+inflacion["MA_5"] = inflacion["Valor"].rolling(5).mean()
+sns.lineplot(data=inflacion, x="Fecha", y="MA_5", legend=False)
+plt.ylabel("Inflación diaria")
+plt.show()
+
+inflacion["MA_8"] = inflacion["Valor"].rolling(8).mean()
+sns.lineplot(data=inflacion, x="Fecha", y="MA_8", legend=False)
+plt.ylabel("Inflación diaria")
+plt.show()
+
+# MA de orden 2 y 8 para la serie de Precipitación mensual
+precipitacion["MA_2"] = precipitacion["Valor"].rolling(2).mean()
+sns.lineplot(data=precipitacion, x="Fecha", y="MA_2", legend=False)
+plt.ylabel("Precipitación mensual")
+plt.show()
+
+precipitacion["MA_8"] = precipitacion["Valor"].rolling(8).mean()
+sns.lineplot(data=precipitacion, x="Fecha", y="MA_8", legend=False)
+plt.ylabel("Precipitación mensual")
+plt.show()
 
 # %%##########
 ## PUNTO 3
@@ -89,5 +112,25 @@ simular_graph_analizar_arma(ar, ma, sigma=np.sqrt(9))
 # %%##########
 ## PUNTO 4
 ##############
-data4 = pd.read_excel("Tarea_feb1/data/HW02-DatosPunto4.xls", sheet_name=0, index_col=0)
-# print(data4)
+data4 = pd.read_excel("./data/HW02-DatosPunto4.xls", sheet_name=0, index_col=0)
+
+#%% Box-Jenkings para la serie 1
+serie1 = data4["serie1"]
+sns.lineplot(data=serie1)
+plt.show()
+
+#%% Box-Jenkings para la serie 2
+serie2 = data4["serie2"]
+sns.lineplot(data=serie2)
+plt.show()
+
+#%% Box-Jenkings para la serie 3
+serie3 = data4["serie3"]
+sns.lineplot(data=serie3)
+plt.show()
+
+#%% Box-Jenkings para la serie 4
+serie4 = data4["serie4"]
+sns.lineplot(data=serie4)
+plt.show()
+
